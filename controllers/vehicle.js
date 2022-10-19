90ddf85bd8922c7beb041e130d3c87067e860ae1
angelolustosa <alessandrofeitoza01@gmail.com>
2022-10-19 11:46:48 -0300

    First commit

---
+++ controllers/vehicle.js
@@ -0,0 +1,22 @@
+function add () {
+    return "Cadastrando Veiculo";
+}
+
+function list() {
+    return "Listando veiculo";
+}
+
+function update() {
+    return "Editando veiculo";
+}
+
+function remove() {
+    return "Excluindo veiculo";
+}
+
+module.exports = {
+    add, 
+    list,
+    update,
+    remove,
+};
